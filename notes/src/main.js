9cd9202980936b75a62bef005c6bb8dd2d74a690
Javor Emir <emir.j2002@gmail.com>
2024-04-25 17:20:47 +0200

    [ADD] insérer note

--- notes/src/main.js
+++ notes/src/main.js
@@ -1,25 +1,38 @@
-const titre = document.getElementById('titre');
-const contenu = document.getElementById('contenu');
+const { invoke } = window.__TAURI__.tauri;
 
-//Creer une note 
-function creerNote(){
+function creerNote() {
+  const titre = document.getElementById("titre").value;
+  const contenu = document.getElementById("contenu").value;
 
-  console.log(titre, contenu)
-    const note = document.createElement('div');
-    note.classList.add('note');
-    note.innerHTML = `
-        <h1>${titre.value}</h1>
-        <p>${contenu.value}</p>
-        <button onclick="supprimerNote()">Supprimer</button>
-    `;
-    document.body.appendChild(note);
-} 
+  const note = document.createElement("div");
+  note.classList.add("bg-white", "p-4", "rounded-lg", "note"); // J'ai ajouté 'note' à la classe pour que la fonction supprimerNote fonctionne correctement
+  note.innerHTML = `
+      <h1 class="font-bold text-lg">${titre}</h1>
+      <p>${contenu}</p>
+      <button onclick="supprimerNote(this)">Supprimer</button>
+  `;
+  document.getElementById("savedNotes").appendChild(note);
+  insererFichierLocal(titre, contenu); // Passez les valeurs directement à la fonction suivante
+}
 
-function supprimerNote(){
-    const notes = document.querySelectorAll('.note');
-    notes.forEach(note => {
-        note.addEventListener('click', () => {
-            note.remove();
-        });
-    });
+async function insererFichierLocal(titre, contenu) {
+  const note = {
+    titre: titre,
+    contenu: contenu,
+  };
+
+  console.log("Debug note object:", note); // Pour vérifier l'objet
+
+  try {
+    const fichier = await invoke("save_note", { note }); // Assurez-vous que l'objet est bien passé en tant que 'note'
+    console.log(fichier);
+  } catch (error) {
+    console.error("Erreur lors de l'invocation de save_note:", error);
+  }
+}
+
+
+
+function supprimerNote(button) {
+  button.parentNode.remove(); // Cela supprime la div de la note entière
 }
